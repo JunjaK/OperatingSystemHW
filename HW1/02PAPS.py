ce94063f8a312eeade18d21742223172f31ff80e
Yoonjunhyeon <yoonjunhyeon@twentyoz.kr>
2020-05-05 20:53:22 +0900

    HW1 종료

---
+++ HW1/02PAPS.py
@@ -0,0 +1,214 @@
+'''
+Gantt Chart Setting
+'''
+# Importing the matplotlb.pyplot
+import matplotlib.pyplot as plt
+from math import gcd
+
+# Declaring a figure "gnt"
+fig, gnt = plt.subplots()
+
+# Setting Y-axis limits
+gnt.set_ylim(0, 55)
+
+# Setting X-axis limits
+gnt.set_xlim(0, 60)
+
+# Setting labels for x-axis and y-axis
+gnt.set_xlabel('time since start')
+gnt.set_ylabel('process')
+
+# Setting ticks on y-axis
+gnt.set_yticks([5, 13, 18, 23, 28, 33, 38, 43, 48])
+# Labelling tickes of y-axis
+gnt.set_yticklabels(['ALL', 'τ1', 'τ2', 'τ3', 'τ4', 'τ5', 'AP1', 'AP2', 'AP3'])
+
+# Setting graph attribute
+gnt.grid(True)
+
+'''
+Polling Server AperiodicProcess Scheduling
+'''
+print('Polling Server APS Average Delay Calculating Program Start! \n')
+# Hyper Parameter Input (Periodic Task)
+print('Please input Periodic task1')
+t1ComputeTime, t1Period = map(int, input(
+    'Periodic task1 computation time, period : ').split())
+t1RemainTask = 0
+
+print('Please input Periodic task2')
+t2ComputeTime, t2Period = map(int, input(
+    'Periodic task2 computation time, period : ').split())
+t2RemainTask = 0
+
+print('Please input Periodic task3')
+t3ComputeTime, t3Period = map(int, input(
+    'Periodic task3 computation time, period : ').split())
+t3RemainTask = 0
+
+print('Please input Periodic task4')
+t4ComputeTime, t4Period = map(int, input(
+    'Periodic task4 computation time, period : ').split())
+t4RemainTask = 0
+
+print('Please input Periodic task5')
+t5ComputeTime, t5Period = map(int, input(
+    'Periodic task5 computation time, period : ').split())
+t5RemainTask = 0
+
+# Hyper Parameter Input (Aperiodic Task)
+print('\nPlease input Aperiodic task1')
+AP1ComputeTime, AP1ArrivalTime = map(int, input(
+    'Aperiodic task1 computation time, period : ').split())
+AP1RemainTask = 0
+
+print('Please input Aperiodic task2')
+AP2ComputeTime, AP2ArrivalTime = map(int, input(
+    'Aperiodic task2 computation time, period : ').split())
+AP2RemainTask = 0
+
+print('Please input Aperiodic task3')
+AP3ComputeTime, AP3ArrivalTime = map(int, input(
+    'Aperiodic task3 computation time, period : ').split())
+AP3RemainTask = 0
+
+print('Please input Polling server task')
+PollingCapacity, PollingPeriod = map(int, input(
+    'Polling capacity, period : ').split())
+
+# Hyper Period Calculating
+PeriodList = [t1Period, t2Period, t3Period, t4Period, t5Period]
+lcm = t1Period
+for i in PeriodList[1:]:
+    lcm = int(lcm*i / gcd(lcm, i))
+
+HyperPeriod = lcm
+
+taskList = [0 for i in range(HyperPeriod)]
+print("\nPeriodic task's Hyper Period :", HyperPeriod)
+
+# Proccess Execution
+for i in range(0, HyperPeriod):
+
+    # Aperiodic Task Arrival
+    if (i == AP1ArrivalTime):
+        AP1RemainTask += AP1ComputeTime
+
+    if (i == AP2ArrivalTime):
+        AP2RemainTask += AP2ComputeTime
+
+    if (i == AP3ArrivalTime):
+        AP3RemainTask += AP3ComputeTime
+
+    # Periodic Task Arrival
+    if (i % t1Period == 0):
+        t1RemainTask += t1ComputeTime
+
+    if (i % t2Period == 0):
+        t2RemainTask += t2ComputeTime
+
+    if (i % t3Period == 0):
+        t3RemainTask += t3ComputeTime
+
+    if (i % t4Period == 0):
+        t4RemainTask += t4ComputeTime
+
+    if (i % t5Period == 0):
+        t5RemainTask += t5ComputeTime
+
+    # Aperiodic Task Execution
+    if (i % PollingPeriod == 0):
+        for j in range(0, PollingCapacity):
+            if (AP1RemainTask == 0 and AP2RemainTask == 0 and AP3RemainTask == 0):
+                break
+            if (AP1RemainTask > 0):
+                if(taskList[i] == 0):
+                    taskList[i] = 'ap1'
+                    gnt.broken_barh([(i, 1)], (4, 2), facecolors=('crimson'))
+                    gnt.broken_barh([(i, 1)], (37, 2), facecolors=('crimson'))
+                    AP1RemainTask -= 1
+
+            if (AP2RemainTask > 0):
+                if(taskList[i] == 0):
+                    taskList[i] = 'ap2'
+                    gnt.broken_barh([(i, 1)], (4, 2), facecolors=('pink'))
+                    gnt.broken_barh([(i, 1)], (42, 2), facecolors=('pink'))
+                    AP2RemainTask -= 1
+
+            if (AP3RemainTask > 0):
+                if(taskList[i] == 0):
+                    taskList[i] = 'ap3'
+                    gnt.broken_barh([(i, 1)], (4, 2), facecolors=('orange'))
+                    gnt.broken_barh([(i, 1)], (47, 2), facecolors=('orange'))
+                    AP3RemainTask -= 1
+            i += 1
+
+    # Periodic Task Execution
+    if (t1RemainTask > 0):
+        if(taskList[i] == 0):
+            taskList[i] = 't1'
+            gnt.broken_barh([(i, 1)], (4, 2), facecolors=('gold'))
+            gnt.broken_barh([(i, 1)], (12, 2), facecolors=('gold'))
+            t1RemainTask -= 1
+
+    if (t2RemainTask > 0):
+        if(taskList[i] == 0):
+            taskList[i] = 't2'
+            gnt.broken_barh([(i, 1)], (4, 2), facecolors=('green'))
+            gnt.broken_barh([(i, 1)], (17, 2), facecolors=('green'))
+            t2RemainTask -= 1
+
+    if (t3RemainTask > 0):
+        if(taskList[i] == 0):
+            taskList[i] = 't3'
+            gnt.broken_barh([(i, 1)], (4, 2), facecolors=('lightgreen'))
+            gnt.broken_barh([(i, 1)], (22, 2), facecolors=('lightgreen'))
+            t3RemainTask -= 1
+
+    if (t4RemainTask > 0):
+        if(taskList[i] == 0):
+            taskList[i] = 't4'
+            gnt.broken_barh([(i, 1)], (4, 2), facecolors=('lightblue'))
+            gnt.broken_barh([(i, 1)], (27, 2), facecolors=('lightblue'))
+            t4RemainTask -= 1
+
+    if (t5RemainTask > 0):
+        if(taskList[i] == 0):
+            taskList[i] = 't5'
+            gnt.broken_barh([(i, 1)], (4, 2), facecolors=('blue'))
+            gnt.broken_barh([(i, 1)], (32, 2), facecolors=('blue'))
+            t5RemainTask -= 1
+
+# Average Delay Time Calculating
+AP1CompleteTime = 0
+AP2CompleteTime = 0
+AP3CompleteTime = 0
+for i in range(0, HyperPeriod):
+    if (taskList[i] == 'ap1'):
+        AP1CompleteTime = i
+    if (taskList[i] == 'ap2'):
+        AP2CompleteTime = i
+    if (taskList[i] == 'ap3'):
+        AP3CompleteTime = i
+AP1DelayTime = AP1CompleteTime - AP1ArrivalTime
+AP2DelayTime = AP2CompleteTime - AP2ArrivalTime
+AP3DelayTime = AP3CompleteTime - AP3ArrivalTime
+
+APSaverageDelayTime = (AP1DelayTime + AP2DelayTime + AP3DelayTime) / 3
+
+print("\n----Polling Server APS Result----")
+
+print("AP1 Complete Time : ", AP1CompleteTime)
+print("AP2 Complete Time : ", AP2CompleteTime)
+print("AP3 Complete Time : ", AP3CompleteTime)
+
+print("\nAP1 Delay Time : ", AP1DelayTime)
+print("AP2 Delay Time : ", AP2DelayTime)
+print("AP3 Delay Time : ", AP3DelayTime)
+
+print("\nAperiodic Proccess Scheduling Average Delay Time : ", APSaverageDelayTime)
+
+'''
+Result Saving to img(png)
+'''
+plt.savefig("PollingServerAPS.png")
---
+++ HW1/readme.md
@@ -0,0 +1,128 @@
+# 프로그램 설명입니다.
+
+## 사용한 자료구조
+#### 프로그램의 구조에 관해서는 주석으로 설명했습니다.
+#### 사용한 주요 변수 설명 (설명에 없는 변수는 단순 계산을 위해 선언한 것입니다.)
+| name | Description                                                      | Type           |
+| --------- | ---------------------------------------------------------------- | -------------- |
+| t1ComputeTime | Periodic task1의 Computation Time을 저장하는 변수입니다. (1~5 동일) | int | -      | 
+| t1Period | Periodic task1의 Period를 저장하는 변수입니다. (1~5 동일) | int         |
+| t1RemainTask | Periodic task1의 남은 Task를 저장하는 변수입니다. task의 period마다 주어지는 task의 Computation time의 합산입니다. (1~5 동일) | int |
+| AP1ComputeTime | Aperiodic task1의 Computation Time을 저장하는 변수입니다. (1~3 동일) | int | -      | 
+| AP1ArrivalTime | Aperiodic task1의 Period를 저장하는 변수입니다. (1~3 동일) | int         |
+| AP1RemainTask | Aperiodic task1의 남은 Task를 저장하는 변수입니다. task의 period마다 주어지는 task의 Computation time의 합산입니다. (1~3 동일) | int |
+| PollingCapacity | Polling server의 Capacity를 저장하는 변수입니다. | int |
+| PollingPeriod | Polling server의 Period를 저장하는 변수입니다. | int |
+| HyperPeriod | Periodic Task들의 Hyper Period를 저장하는 변수입니다. | int |
+| taskList | 각 시간별, Task들이 작업을 수행한 시간을 저장해 놓은 리스트입니다.  | array/string |
+| APSaverageDelayTime | Aperiodic Process Scheduling의 평균 지연 시간을 계산한 결과를 저장한 변수입니다.  | float |
+
+
+
+
+## 가정사항
+#### 1. Periodic task는 5개만을 입력 받습니다. (Gantt chart 만들기 위해 고정할 필요가 있습니다. - 색 지정에 관한 문제)
+#### 2. Aperiodic task는 3개만을 입력 받습니다. (가정사항 1과 동일한 이유.)
+#### 3. 각 Periodic task들은 period가 짧은 순으로 입력 받습니다.
+#### 4. 각 Aperiodic task들은 arrival time이 빠른 순으로 입력 받습니다.
+
+## 사용방법
+입력의 경우 구분은 공백으로 합니다.
+#### 0. 관련 가상환경 활성화 및 모듈 설치 
+```sh init```
+#### 1. Background APS 실행의 경우 
+```python 01BAPS.py```
+- Periodic task 5개를 입력합니다. (Computation time, Period 순으로 입력)
+- Aperiodic task 3개를 입력합니다. (Computation time, Arrival Time 순으로 입력)
+- 결과를 확인합니다.
+#### 2. Polling server APS 실행의 경우
+```python 02PAPS.py```
+- Periodic task 5개를 입력합니다. (Computation time, Period 순으로 입력) 
+- Aperiodic task 3개를 입력합니다. (Computation time, Arrival Time 순으로 입력)
+- Polling Server 정보를 입력합니다. (Capacity, Period 순으로 입력)
+#### ps. sh init이 제대로 실행 안될 경우, 아래의 명령어를 차례로 입력해주십시오.
+```
+source env/bin/activate
+pip install -r requirements.txt
+```
+#### ps2. 만약 ```pip install -r requirements.txt```가 정상 실행되지 않는다면 아래의 모듈을 인스톨 해주십시오.
+```
+pip install matplotlib
+pip install numpy
+```
+#### 각 프로그램 실행 후, Gantt Chart는 png 파일로 생성됩니다.
+
+## 입출력 예시
+#### 예1, Background APS 
+```
+Background APS Average Delay Calculating Program Start! 
+
+Please input Periodic task1
+Periodic task1 computation time, period : 1 5
+Please input Periodic task2
+Periodic task2 computation time, period : 1 10
+Please input Periodic task3
+Periodic task3 computation time, period : 1 15
+Please input Periodic task4
+Periodic task4 computation time, period : 3 30
+Please input Periodic task5
+Periodic task5 computation time, period : 5 60
+
+Please input Aperiodic task1
+Aperiodic task1 computation time, period : 2 7
+Please input Aperiodic task2
+Aperiodic task2 computation time, period : 1 10
+Please input Aperiodic task3
+Aperiodic task3 computation time, period : 1 29
+
+Periodic task's Hyper Period : 60
+
+----Background APS Result----
+AP1 Complete Time :  17
+AP2 Complete Time :  18
+AP3 Complete Time :  29
+
+AP1 Delay Time :  10
+AP2 Delay Time :  8
+AP3 Delay Time :  0
+
+Aperiodic Proccess Scheduling Average Delay Time :  6.0
+```
+
+#### 예2, Polling Server APS
+```
+Polling Server APS Average Delay Calculating Program Start! 
+
+Please input Periodic task1
+Periodic task1 computation time, period : 1 5
+Please input Periodic task2
+Periodic task2 computation time, period : 1 10
+Please input Periodic task3
+Periodic task3 computation time, period : 1 15
+Please input Periodic task4
+Periodic task4 computation time, period : 3 30
+Please input Periodic task5
+Periodic task5 computation time, period : 5 60
+
+Please input Aperiodic task1
+Aperiodic task1 computation time, period : 2 7
+Please input Aperiodic task2
+Aperiodic task2 computation time, period : 1 10
+Please input Aperiodic task3
+Aperiodic task3 computation time, period : 1 29
+Please input Polling server task
+Polling capacity, period : 1 5
+
+Periodic task's Hyper Period : 60
+
+----Polling Server APS Result----
+AP1 Complete Time :  15
+AP2 Complete Time :  20
+AP3 Complete Time :  30
+
+AP1 Delay Time :  8
+AP2 Delay Time :  10
+AP3 Delay Time :  1
+
+Aperiodic Proccess Scheduling Average Delay Time :  6.333333333333333
+``` 
